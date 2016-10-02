33dea89e4a0e7ff4dd216c170e1cc5081b1f4935
Moises Hiraldo <moiseshiraldo@gmail.com>
2016-10-02 17:23:08 +0100

    Abort rollback if getting migrations fail

--- south-post-checkout-rollback.sh
+++ south-post-checkout-rollback.sh
@@ -2,7 +2,7 @@
 # Roll back migrations from the previous branch.
 
 # Use this variable if you want to roll back migrations to a safe branch
-MASTER_BRANCH="production"
+MASTER_BRANCH=""
 
 OLD_BRANCH=$1
 NEW_BRANCH=$2
@@ -10,9 +10,9 @@ IS_BRANCH_CHANGE=$3
 
 rollback () {
     if [ -z "${current_migrations[$app]}" ] && [ $last_migration -gt 0 ]; then
-        python -Wi manage.py migrate $app zero
+        python manage.py migrate $app zero
     elif [ ${current_migrations[$app]} -lt $last_migration ]; then
-        python -Wi manage.py migrate $app ${current_migrations[$app]}
+        python manage.py migrate $app ${current_migrations[$app]}
     elif [ ${current_migrations[$app]} -gt $last_migration ]; then
         echo "Warning: the previous branch has missing migrations for" $app
     fi
@@ -23,8 +23,12 @@ if [ $NEW_BRANCH == $OLD_BRANCH ] || [ -n "$SKIP_POST_ROLLBACK" ]; then
 fi
 
 if [ $IS_BRANCH_CHANGE -eq 1 ]; then
-    declare -A current_migrations
     migrations="$(python -Wi manage.py migrate --list)"
+    # Exit if we could not get migrations
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+    declare -A current_migrations
     new_branch="$(git rev-parse --abbrev-ref HEAD)"
     
     # Checkout master branch
@@ -33,6 +37,7 @@ if [ $IS_BRANCH_CHANGE -eq 1 ]; then
     fi
     
     # Get last migration on current branch for every app
+    last_migration="0000"
     while read -r line; do
         array=( $line )
         if [ -z ${array[0]} ] && [ -v app ]; then
@@ -50,10 +55,14 @@ if [ $IS_BRANCH_CHANGE -eq 1 ]; then
     
     # Checkout old branch
     SKIP_POST_ROLLBACK=1 git checkout -q -b tmp-south-rollback $OLD_BRANCH
-    new_migrations="$(python -Wi manage.py migrate --list)"
+    previous_migrations="$(python -Wi manage.py migrate --list)"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     unset app
     
     # Get old migrations and roll back if necessary
+    last_migration="0000"
     while read -r line; do
         array=( $line )
         if [ -z ${array[0]} ] && [ -v app ]; then
@@ -66,7 +75,7 @@ if [ $IS_BRANCH_CHANGE -eq 1 ]; then
         else
             app=${array[0]}
         fi
-    done <<< "$new_migrations"
+    done <<< "$previous_migrations"
     rollback
     
     # Checkout current branch again
